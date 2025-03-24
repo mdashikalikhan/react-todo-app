5564af9e19960afa035a99d630d5fe1a373e7f21
mdashikalikhan <khan.ashik@gmail.com>
2025-03-24 15:08:07 +0600

    state...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import TodoTable from './components/TodoTable';
+import { useState } from 'react';
+
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = useState([
     {rowSerial:1, rowDescription: 'Description 1', rowAssigned: 'Assigned 1'},
     {rowSerial:2, rowDescription: 'Description 2', rowAssigned: 'Assigned 2'},
     {rowSerial:3, rowDescription: 'Description 3', rowAssigned: 'Assigned 3'},
     {rowSerial:4, rowDescription: 'Description 4', rowAssigned: 'Assigned 4'}
-  ];
+  ]);
+
+  const addTodo = ()=>{
+    /* console.log('todo button has been clicked!'); */
+    let sl = 1
+    if(todos.length>0){
+      sl = todos.length + 1;
+    }
+    const newTodo = {
+      rowSerial:sl, rowDescription: 'Description ' + sl, rowAssigned: 'Assigned ' + sl
+    };
+
+    setTodos([...todos, newTodo]);
+  }
+
   return (
     <div className='mt-5 container'>
 
@@ -17,6 +33,9 @@ function App() {
         </div>
         <div className="card-body">
           <TodoTable todos={todos}/>
+          <button className='btn btn-primary' onClick={()=>{addTodo()}}>
+            Add new todo
+          </button>
         </div>
       </div>
 
